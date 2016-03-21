fdf58e93fac77d41f7aa8b1b07f890b3a6be1673
manfred <manfredHuFE@gmail.com>
2016-03-21 16:32:46 +0800

    fix one bug

---
+++ app/Gruntfile.js
@@ -0,0 +1,35 @@
+// Generated on 2015-11-29 using generator-angular 0.14.0
+'use strict';
+
+
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        watch: {
+            options: {
+                livereload: true
+            },
+            all: {
+                files: ['views/*','static/*']
+            }
+        },
+        nodemon: {
+            dev: {
+                script: './app.js'
+            }
+        },
+        concurrent: {
+            options: {
+                logConcurrentOutput: true
+            },
+            dev: {
+                tasks: ['nodemon', 'watch']
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-concurrent');
+    grunt.loadNpmTasks('grunt-nodemon');
+    grunt.registerTask('default', ['concurrent']);
+};
